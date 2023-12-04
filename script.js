591dbe4ae9c13c82eaddd16bd4bef3626d942c8c
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-04 10:03:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function(){
+	var dropdown = document.getElementById("colorSelect");
+	var selectedIndex = dropdown.selectedIndex;
+
+	if(selectedIndex !== -1)
+	{
+		dropdown.remove(selectedIndex);
+	}
+});
